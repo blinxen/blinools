b2a6f2118af6793b3d1798512dd9daed908e3169
blinxen <h-k-81@hotmail.com>
2026-09-02 10:13:21 +0200

    Make microvm kernel + rootfs more stable / compatible

--- scripts/fedora-rootfs.sh
+++ scripts/fedora-rootfs.sh
@@ -4,7 +4,7 @@ set -eux
 ROOTFS_IMG="$(pwd)/rootfs.img"
 ROOTFS="$(pwd)/rootfs"
 
-podman build -f Dockerfile.fedora -t fedora-microvm-rootfs .
+podman build -f Dockerfile.fedora --target base -t fedora-microvm-rootfs .
 
 podman create --name export-tmp fedora-microvm-rootfs
 podman export export-tmp -o rootfs.tar
@@ -16,7 +16,9 @@ mkfs.ext4 -L rootfs -F "${ROOTFS_IMG}"
 sudo rm -rf tmp
 mkdir tmp
 sudo mount "${ROOTFS_IMG}" tmp
-sudo tar -xpf rootfs.tar -C tmp --xattrs --xattrs-include='*' --numeric-owner
+sudo tar -xpf rootfs.tar -C tmp --xattrs --xattrs-include='*' --numeric-owner --exclude='.dockerenv' --exclude='/run/.containerenv'
 sudo umount tmp
 sudo rm -rf tmp
-chmod -w rootfs.img
+docker build -f Dockerfile.fedora --target kernel --output type=local,dest=./out .
+mv out/kernel .
+rmdir out
