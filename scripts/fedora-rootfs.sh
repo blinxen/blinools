122d102050bead43c5df38553b59bdf086cdb33e
blinxen <h-k-81@hotmail.com>
2026-09-02 21:18:53 +0200

    Make rootfs read-only after it is built and remove dummy resolv.conf so systemd can recreate it correctly

--- scripts/fedora-rootfs.sh
+++ scripts/fedora-rootfs.sh
@@ -10,6 +10,7 @@ podman create --name export-tmp fedora-microvm-rootfs
 podman export export-tmp -o rootfs.tar
 podman rm export-tmp
 
+chmod +w "${ROOTFS_IMG}"
 truncate -s 5G "${ROOTFS_IMG}"
 mkfs.ext4 -L rootfs -F "${ROOTFS_IMG}"
 
@@ -17,8 +18,10 @@ sudo rm -rf tmp
 mkdir tmp
 sudo mount "${ROOTFS_IMG}" tmp
 sudo tar -xpf rootfs.tar -C tmp --xattrs --xattrs-include='*' --numeric-owner --exclude='.dockerenv' --exclude='/run/.containerenv'
+sudo rm -f ./tmp/etc/resolv.conf
 sudo umount tmp
 sudo rm -rf tmp
+chmod -w "${ROOTFS_IMG}"
 docker build -f Dockerfile.fedora --target kernel --output type=local,dest=./out .
 mv out/kernel .
 rmdir out
